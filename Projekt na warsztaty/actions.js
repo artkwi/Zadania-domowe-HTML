f5126491c97b81761520561ba8ef639fd77924d4
artkwi <artkwi@st.amu.edu.pl>
2017-05-29 16:27:39 +0200

    Dodanie js

---
+++ Projekt na warsztaty/actions.js
@@ -0,0 +1,13 @@
+﻿var elementAddButton = document.querySelector('button');
+elementAddButton.addEventListener('click',addElement,false);
+
+function addElement() {
+	var positiontList = document.querySelector(".unordered-list");
+	var newElementLi = document.createElement('li');
+	var newText = document.createTextNode('Treść zadania');
+	newElementLi.appendChild(newText);
+	positiontList.appendChild(newElementLi);
+	console.log('Działa!');
+}
+
+//addElement();
