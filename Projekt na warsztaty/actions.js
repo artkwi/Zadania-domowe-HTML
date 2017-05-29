bedfe0e58c826d40f9cf39c63a6fed28c2d1187f
artkwi <artkwi@st.amu.edu.pl>
2017-05-29 17:57:51 +0200

    Funkcja dodająca zadanie

--- Projekt na warsztaty/actions.js
+++ Projekt na warsztaty/actions.js
@@ -4,10 +4,19 @@ elementAddButton.addEventListener('click',addElement,false);
 function addElement() {
 	var positiontList = document.querySelector(".unordered-list");
 	var newElementLi = document.createElement('li');
-	var newText = document.createTextNode('Treść zadania');
-	newElementLi.appendChild(newText);
+	var newElementButton = document.createElement('button');
+	var newTextButton = document.createTextNode('Usuń');
+	var newElementCheckbox = document.createElement('input');
+	newElementCheckbox.type = "checkbox";
+	var newElementTextBox = document.getElementsByName('text-zadanie');
+	var newText = document.createTextNode(newElementTextBox[0].value);
+	//console.log(newElementTextBox[0].value);
+	
+	newElementLi.appendChild(newElementButton);
+	newElementLi.appendChild(newElementCheckbox);
+	newElementButton.appendChild(newTextButton);
 	positiontList.appendChild(newElementLi);
+	newElementLi.appendChild(newText);
 	console.log('Działa!');
 }
 
-//addElement();
